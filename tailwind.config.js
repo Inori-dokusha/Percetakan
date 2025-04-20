87a78ad4bf5a5c9840479104813d5fbd00587b01
inori.dokusha <inori.dokusha@gmail.com>
2025-04-20 14:54:32 +0000

    update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Work", "sans-serif"],
+      serif: ["Nunito", "serif"],
+    },
+    extend: {
+      colors: {
+        cyan: "#00b8e6",
+        magenta: "#fa1671",
+        yellow: "#ffce1c",
+        black: "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
